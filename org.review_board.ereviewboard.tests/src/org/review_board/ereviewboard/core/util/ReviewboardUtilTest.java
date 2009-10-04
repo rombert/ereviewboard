1116ea8dc935f26963310977984d4903a5738ce9
Markus Knittig <markus@myd0.de>
2009-10-04 23:20:20 +0200

    Tests for ReviewboardUtil.cloneEntity

--- org.review_board.ereviewboard.tests/src/org/review_board/ereviewboard/core/util/ReviewboardUtilTest.java
+++ org.review_board.ereviewboard.tests/src/org/review_board/ereviewboard/core/util/ReviewboardUtilTest.java
@@ -1,23 +1,40 @@
 package org.review_board.ereviewboard.core.util;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import junit.framework.TestCase;
 
 public class ReviewboardUtilTest extends TestCase {
 
+    public void testCloneEntitySimple() {
+        CustomClass testObject = buildCustomClassObject();
+        assertEquals(testObject, ReviewboardUtil.cloneEntity(testObject));
+    }
+
+    public void testCloneEntityDeep() {
+        CustomClass deepTestObject = buildCustomClassObject();
+        CustomClass testObject = new CustomClass();
+        testObject.setCustomObject(deepTestObject);
+        assertEquals(testObject.getCustomObject(),
+                ReviewboardUtil.cloneEntity(testObject).getCustomObject());
+    }
+
+    private CustomClass buildCustomClassObject() {
+        CustomClass testObject = new CustomClass();
+        testObject.setInteger(123);
+        testObject.setString("abc");
+        return testObject;
+    }
+
     public void testJoinListWithInteger() {
         List<Integer> list = Arrays.asList(new Integer[] { 1, 2, 3 });
         assertEquals("1, 2, 3", ReviewboardUtil.joinList(list));
-    }
-
-    private static class CustomClass {
-        @Override
-        public String toString() {
-            return "bla";
-        }
     }
 
     public void testJoinListWithCustomClass() {
@@ -25,6 +42,61 @@ public class ReviewboardUtilTest extends TestCase {
         list.add(new CustomClass());
         list.add(new CustomClass());
         assertEquals("bla, bla", ReviewboardUtil.joinList(list));
+    }
+
+    private static class CustomClass implements Serializable {
+        private String string;
+        private int integer;
+        private CustomClass customObject;
+
+        public String getString() {
+            return string;
+        }
+
+        public void setString(String string) {
+            this.string = string;
+        }
+
+        public int getInteger() {
+            return integer;
+        }
+
+        public void setInteger(int integer) {
+            this.integer = integer;
+        }
+
+        public CustomClass getCustomObject() {
+            return customObject;
+        }
+
+        public void setCustomObject(CustomClass customObject) {
+            this.customObject = customObject;
+        }
+
+        @Override
+        public int hashCode() {
+            return new HashCodeBuilder()
+                    .append(integer)
+                    .append(string)
+                    .toHashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof CustomClass) {
+                CustomClass toCompare = (CustomClass) obj;
+                return new EqualsBuilder()
+                        .append(integer, toCompare.getInteger())
+                        .append(string, toCompare.getString())
+                        .isEquals();
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "bla";
+        }
     }
 
 }
