97179c8879935035b5f40a4172ad9327559b312d
Markus Knittig <markus@myd0.de>
2010-03-20 02:30:17 +0100

    Removed unneseccary imports

--- org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/dialog/CloseReviewRequestDialog.java
+++ org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/dialog/CloseReviewRequestDialog.java
@@ -39,9 +39,7 @@ package org.review_board.ereviewboard.ui.dialog;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
