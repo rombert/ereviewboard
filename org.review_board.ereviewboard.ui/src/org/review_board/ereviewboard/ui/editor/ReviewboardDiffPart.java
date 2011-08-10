bada382e4733db913ee93edffeb7aee4900c456a
Robert Munteanu <robert.munteanu@gmail.com>
2011-08-10 12:29:42 +0300

    Basic support for opening diffs in a Patch viewer
    Not really happy with how this has turned out, we reaching too
    much into internals and most likely this will not be reusable when
    we want to add the possibility to comment.
    
    Hopefully I will be able to revisit soon.
    
    Affects #49: Open changed files in compare editor

--- org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ReviewboardDiffPart.java
+++ org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ReviewboardDiffPart.java
@@ -1,30 +1,49 @@
 package org.review_board.ereviewboard.ui.editor;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.IContentChangeListener;
+import org.eclipse.compare.IContentChangeNotifier;
+import org.eclipse.compare.internal.core.patch.FileDiffResult;
+import org.eclipse.compare.internal.core.patch.FilePatch2;
+import org.eclipse.compare.internal.core.patch.HunkResult;
+import org.eclipse.compare.internal.patch.HunkDiffNode;
+import org.eclipse.compare.internal.patch.PatchCompareEditorInput;
+import org.eclipse.compare.internal.patch.PatchFileDiffNode;
+import org.eclipse.compare.internal.patch.UnmatchedHunkTypedElement;
+import org.eclipse.compare.internal.patch.WorkspacePatcher;
+import org.eclipse.compare.structuremergeviewer.DiffNode;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.mylyn.internal.tasks.ui.editors.EditorUtil;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
 import org.eclipse.mylyn.tasks.ui.editors.AbstractTaskEditorPart;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IEditorRegistry;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.events.HyperlinkAdapter;
 import org.eclipse.ui.forms.events.HyperlinkEvent;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.eclipse.ui.forms.widgets.Section;
-import org.eclipse.ui.part.FileEditorInput;
 import org.review_board.ereviewboard.core.ReviewboardAttributeMapper;
+import org.review_board.ereviewboard.core.ReviewboardCorePlugin;
+import org.review_board.ereviewboard.core.client.ReviewboardClient;
+import org.review_board.ereviewboard.core.exception.ReviewboardException;
 import org.review_board.ereviewboard.core.util.ResourceUtil;
 import org.review_board.ereviewboard.ui.ReviewboardUiPlugin;
 
@@ -73,18 +92,26 @@ public class ReviewboardDiffPart extends AbstractTaskEditorPart {
                     }
                     IFile file = (IFile) resource;
                     
+                    CompareConfiguration configuration = new CompareConfiguration();
+                    WorkspacePatcher patcher = new WorkspacePatcher(file);
+                    ReviewBoardInput input = new ReviewBoardInput(patcher, configuration);
+                    ReviewboardClient client = ReviewboardCorePlugin.getDefault().getConnector().getClientManager().getClient(new TaskRepository(ReviewboardCorePlugin.PLUGIN_ID, getTaskData().getRepositoryUrl()));
                     try {
-                        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-                        IEditorRegistry editorRegistry = PlatformUI.getWorkbench().getEditorRegistry();
-                        
-                        IEditorDescriptor desc = editorRegistry.getDefaultEditor(file.getName());
-                        if ( desc == null )
-                            desc = editorRegistry.findEditor(IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID);
-                            
-                        page.openEditor(new FileEditorInput(file), desc.getId());
-                    } catch (PartInitException e1) {
-                        throw new RuntimeException(e1);
+                        // TODO move to a Job
+                        byte[] rawFileDiff = client.getRawFileDiff(Integer.parseInt(getTaskData().getTaskId()), Integer.parseInt( attribute.getValue()), Integer.parseInt(child.getValue()), new NullProgressMonitor());
+                        patcher.parse(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(rawFileDiff))));
+                    } catch (NumberFormatException e1) {
+                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
+                        return;
+                    } catch (ReviewboardException e1) {
+                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
+                        return;
+                    } catch (IOException e1) {
+                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
+                        return;
                     }
+                    
+                    CompareUI.openCompareEditor(input);
                 }
             });
         }
@@ -94,4 +121,62 @@ public class ReviewboardDiffPart extends AbstractTaskEditorPart {
         setSection(toolkit, section);
     }
     
+    private static class ReviewBoardInput extends PatchCompareEditorInput {
+
+        public ReviewBoardInput(WorkspacePatcher patcher, CompareConfiguration configuration) {
+            super(patcher, configuration);
+        }
+
+        @Override
+        protected void fillContextMenu(IMenuManager manager) {
+
+        }
+        
+        @Override
+        protected Object prepareInput(IProgressMonitor monitor) throws InvocationTargetException,
+                InterruptedException {
+            
+            // Refresh the patcher state
+            getPatcher().refresh();
+            
+            // Build the diff tree
+            processDiffs(getPatcher().getDiffs());
+            
+            return  super.prepareInput(monitor);
+        }
+        
+        // Copied from superclass
+        private void processDiffs(FilePatch2[] diffs) { 
+            for (int i = 0; i < diffs.length; i++) {
+                processDiff(diffs[i], getRoot());
+            }
+        }
+        
+        // Copied from superclass
+        private void processDiff(FilePatch2 diff, DiffNode parent) {
+            FileDiffResult diffResult = getPatcher().getDiffResult(diff);
+            PatchFileDiffNode node = PatchFileDiffNode.createDiffNode(parent, diffResult);
+            HunkResult[] hunkResults = diffResult.getHunkResults();
+            for (int i = 0; i < hunkResults.length; i++) {
+                HunkResult hunkResult = hunkResults[i];
+                if (!hunkResult.isOK()) {
+                    HunkDiffNode hunkNode = HunkDiffNode.createDiffNode(node, hunkResult, true);
+                    Object left = hunkNode.getLeft();
+                    if (left instanceof UnmatchedHunkTypedElement) {
+                        UnmatchedHunkTypedElement element = (UnmatchedHunkTypedElement) left;
+                        element.addContentChangeListener(new IContentChangeListener() {
+                            public void contentChanged(IContentChangeNotifier source) {
+                                if (getViewer() == null || getViewer().getControl().isDisposed())
+                                    return;
+                                getViewer().refresh(true);
+                            }
+                        });
+                    }
+                } else if (isShowMatched()) {
+                    HunkDiffNode.createDiffNode(node, hunkResult, false, true, false);
+                }
+            }
+        }
+    }
+
 }
