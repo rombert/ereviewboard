a85064ea88f83cfb7a5662acbcbfaf252741a56f
Robert Munteanu <robert.munteanu@gmail.com>
2011-08-11 14:44:42 +0300

    Move diff retrieval into the compare input class
    Also extracted all the restricted access parts into ReviewBoardInput.
    
    Affects #49: Open changed files in compare editor

--- org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ReviewboardDiffPart.java
+++ org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ReviewboardDiffPart.java
@@ -13,34 +13,15 @@ package org.review_board.ereviewboard.ui.editor;
 import static org.review_board.ereviewboard.core.ReviewboardAttributeMapper.Attribute.SOURCE_FILE;
 import static org.review_board.ereviewboard.core.ReviewboardAttributeMapper.Attribute.SOURCE_REVISION;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.CompareUI;
-import org.eclipse.compare.IContentChangeListener;
-import org.eclipse.compare.IContentChangeNotifier;
-import org.eclipse.compare.internal.core.patch.FileDiffResult;
-import org.eclipse.compare.internal.core.patch.FilePatch2;
-import org.eclipse.compare.internal.core.patch.HunkResult;
-import org.eclipse.compare.internal.patch.HunkDiffNode;
-import org.eclipse.compare.internal.patch.PatchCompareEditorInput;
-import org.eclipse.compare.internal.patch.PatchFileDiffNode;
-import org.eclipse.compare.internal.patch.UnmatchedHunkTypedElement;
-import org.eclipse.compare.internal.patch.WorkspacePatcher;
-import org.eclipse.compare.structuremergeviewer.DiffNode;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.mylyn.internal.tasks.ui.editors.EditorUtil;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
@@ -56,7 +37,6 @@ import org.eclipse.ui.forms.widgets.Section;
 import org.review_board.ereviewboard.core.ReviewboardCorePlugin;
 import org.review_board.ereviewboard.core.ReviewboardDiffMapper;
 import org.review_board.ereviewboard.core.client.ReviewboardClient;
-import org.review_board.ereviewboard.core.exception.ReviewboardException;
 import org.review_board.ereviewboard.core.util.ResourceUtil;
 import org.review_board.ereviewboard.ui.ReviewboardUiPlugin;
 
@@ -103,26 +83,12 @@ public class ReviewboardDiffPart extends AbstractTaskEditorPart {
                         ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.WARNING, ReviewboardUiPlugin.PLUGIN_ID, "Unable to find a matching file for " + child.getValue() + " tried " + paths ));
                         return;
                     }
+                    
                     IFile file = (IFile) resource;
                     
                     CompareConfiguration configuration = new CompareConfiguration();
-                    WorkspacePatcher patcher = new WorkspacePatcher(file);
-                    ReviewBoardInput input = new ReviewBoardInput(patcher, configuration);
                     ReviewboardClient client = ReviewboardCorePlugin.getDefault().getConnector().getClientManager().getClient(new TaskRepository(ReviewboardCorePlugin.PLUGIN_ID, getTaskData().getRepositoryUrl()));
-                    try {
-                        // TODO move to a Job
-                        byte[] rawFileDiff = client.getRawFileDiff(Integer.parseInt(getTaskData().getTaskId()), diffMapper.getDiffRevision(), Integer.parseInt(child.getValue()), new NullProgressMonitor());
-                        patcher.parse(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(rawFileDiff))));
-                    } catch (NumberFormatException e1) {
-                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
-                        return;
-                    } catch (ReviewboardException e1) {
-                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
-                        return;
-                    } catch (IOException e1) {
-                        ReviewboardUiPlugin.getDefault().getLog().log(new Status(Status.ERROR, ReviewboardUiPlugin.PLUGIN_ID, "Failed loading diff ", e1));
-                        return;
-                    }
+                    ReviewBoardInput input = new ReviewBoardInput(file, configuration, client, Integer.parseInt(getTaskData().getTaskId()), diffMapper.getDiffRevision(), Integer.parseInt(child.getValue()));
                     
                     CompareUI.openCompareEditor(input);
                 }
@@ -132,64 +98,6 @@ public class ReviewboardDiffPart extends AbstractTaskEditorPart {
         toolkit.paintBordersFor(composite);
         section.setClient(composite);
         setSection(toolkit, section);
-    }
-    
-    private static class ReviewBoardInput extends PatchCompareEditorInput {
-
-        public ReviewBoardInput(WorkspacePatcher patcher, CompareConfiguration configuration) {
-            super(patcher, configuration);
-        }
-
-        @Override
-        protected void fillContextMenu(IMenuManager manager) {
-
-        }
-        
-        @Override
-        protected Object prepareInput(IProgressMonitor monitor) throws InvocationTargetException,
-                InterruptedException {
-            
-            // Refresh the patcher state
-            getPatcher().refresh();
-            
-            // Build the diff tree
-            processDiffs(getPatcher().getDiffs());
-            
-            return  super.prepareInput(monitor);
-        }
-        
-        // Copied from superclass
-        private void processDiffs(FilePatch2[] diffs) { 
-            for (int i = 0; i < diffs.length; i++) {
-                processDiff(diffs[i], getRoot());
-            }
-        }
-        
-        // Copied from superclass
-        private void processDiff(FilePatch2 diff, DiffNode parent) {
-            FileDiffResult diffResult = getPatcher().getDiffResult(diff);
-            PatchFileDiffNode node = PatchFileDiffNode.createDiffNode(parent, diffResult);
-            HunkResult[] hunkResults = diffResult.getHunkResults();
-            for (int i = 0; i < hunkResults.length; i++) {
-                HunkResult hunkResult = hunkResults[i];
-                if (!hunkResult.isOK()) {
-                    HunkDiffNode hunkNode = HunkDiffNode.createDiffNode(node, hunkResult, true);
-                    Object left = hunkNode.getLeft();
-                    if (left instanceof UnmatchedHunkTypedElement) {
-                        UnmatchedHunkTypedElement element = (UnmatchedHunkTypedElement) left;
-                        element.addContentChangeListener(new IContentChangeListener() {
-                            public void contentChanged(IContentChangeNotifier source) {
-                                if (getViewer() == null || getViewer().getControl().isDisposed())
-                                    return;
-                                getViewer().refresh(true);
-                            }
-                        });
-                    }
-                } else if (isShowMatched()) {
-                    HunkDiffNode.createDiffNode(node, hunkResult, false, true, false);
-                }
-            }
-        }
     }
 
 }
