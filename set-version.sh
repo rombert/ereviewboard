4957585a8bbe1c1aaabd62f211848cb339ab4d81
Robert Munteanu <robert.munteanu@gmail.com>
2011-09-05 10:13:14 +0300

    Add set-version.sh
    Copied from the mylyn-mantis repository

---
+++ set-version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+if [ $# -ne 1 ] ; then
+		echo "Usage: $0 version";
+		exit 1;
+fi
+
+newVersion=$1
+
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$newVersion
+
+find -name MANIFEST.MF | xargs dos2unix
+find -name site.xml | xargs sed -i 's#feature url="com#feature url="features/com#' 
+
