04c8b36dbd9281adab72f7c20a574362ae39c1d8
Robert Munteanu <robert@lmn.ro>
2011-10-14 01:52:31 +0300

    Organised imports

--- org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/client/RestfulReviewboardClient.java
+++ org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/client/RestfulReviewboardClient.java
@@ -56,7 +56,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.TimeZone;
 
