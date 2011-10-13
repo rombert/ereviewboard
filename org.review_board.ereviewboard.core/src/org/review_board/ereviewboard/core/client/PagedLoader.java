b197961333bc0ffeb3f9d32c131630e382403a09
Robert Munteanu <robert.munteanu@gmail.com>
2011-10-13 17:21:04 +0300

    Trace operations of PagedLoader

--- org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/client/PagedLoader.java
+++ org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/client/PagedLoader.java
@@ -16,6 +16,8 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.mylyn.commons.net.Policy;
+import org.review_board.ereviewboard.core.ReviewboardCorePlugin;
+import org.review_board.ereviewboard.core.TraceLocation;
 import org.review_board.ereviewboard.core.exception.ReviewboardException;
 
 /**
@@ -35,12 +37,14 @@ public abstract class PagedLoader<T> {
     private int limit;
     
     public PagedLoader(int increment, IProgressMonitor monitor, String progressMessage) {
+        
         this.increment = increment;
         this.monitor = monitor;
         this.progressMessage = progressMessage;
     }
 
     protected abstract PagedResult<T> doLoadInternal(int start, int maxResults, IProgressMonitor monitor) throws ReviewboardException;
+
 
     public List<T> doLoad() throws ReviewboardException {
         
@@ -50,6 +54,8 @@ public abstract class PagedLoader<T> {
         int currentPage = 0;
         int actualLimit = 0;
         int stillToLoad = 0;
+        
+        ReviewboardCorePlugin.getDefault().trace(TraceLocation.SYNC, this + " starting run.");
         
         while ( true ) {
 
@@ -62,6 +68,9 @@ public abstract class PagedLoader<T> {
 
             // we perform the monitor work ourselves, so pass a NPM downstream            
             PagedResult<T> pagedUsers = doLoadInternal(currentPage * increment, adjustedIncrement, new NullProgressMonitor());
+            
+            ReviewboardCorePlugin.getDefault().trace(TraceLocation.SYNC, this + " page : " + currentPage + ": loaded " + pagedUsers.getResults().size() + " elements.");
+            
             if ( results == null ) {
                 results = new ArrayList<T>(pagedUsers.getTotalResults());
                 monitor.beginTask(progressMessage, pagedUsers.getTotalResults());
@@ -71,14 +80,14 @@ public abstract class PagedLoader<T> {
                     actualLimit = Math.min(limit, pagedUsers.getTotalResults());
             }
             
-            
-            
             Policy.advance(monitor, pagedUsers.getResults().size());
             results.addAll(pagedUsers.getResults());
             
             currentPage++;
             
             stillToLoad = actualLimit - results.size();
+            
+            ReviewboardCorePlugin.getDefault().trace(TraceLocation.SYNC, this + " page : " + currentPage + ": still to load: " + stillToLoad);
             
             if ( stillToLoad == 0 )
                 break;
@@ -89,18 +98,26 @@ public abstract class PagedLoader<T> {
             }
         }
         
+        ReviewboardCorePlugin.getDefault().trace(TraceLocation.SYNC, this + " completed run.");
+        
         monitor.done();
         
         return results;
     }
+    
 
     public void setLimit(int limit) {
-        
         
         if ( limit <= 0 )
             throw new IllegalArgumentException("limit must be > 0");
         
         this.limit = limit;
         
+    }
+    
+    @Override
+    public String toString() {
+        
+        return getClass().getSimpleName() + " [ " + progressMessage + " ]";
     }
 }
