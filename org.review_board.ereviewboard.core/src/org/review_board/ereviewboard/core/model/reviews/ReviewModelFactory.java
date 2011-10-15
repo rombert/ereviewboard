d559515fee3ec797dfce988097508a1717f56635
Robert Munteanu <robert@lmn.ro>
2011-10-15 17:16:44 +0300

    Ensure that diff comments are displayed in the correct order
    Affects #6 ( Display file comments in compare editor )

--- org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/model/reviews/ReviewModelFactory.java
+++ org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/model/reviews/ReviewModelFactory.java
@@ -1,7 +1,9 @@
 package org.review_board.ereviewboard.core.model.reviews;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.mylyn.reviews.core.model.*;
 import org.eclipse.mylyn.reviews.internal.core.model.ReviewsFactory;
@@ -63,7 +65,11 @@ public class ReviewModelFactory {
     
     public void appendComments(IFileItem fileItem, List<DiffComment> diffComments, DiffCommentLineMapper diffCommentLineMapper) {
         
-        for ( DiffComment diffComment : diffComments ) {
+        List<DiffComment> sortedDiffComments = new ArrayList<DiffComment>(diffComments);
+        
+        Map<Range, ITopic> rangeToTopics = new HashMap<Range, ITopic>();
+        
+        for ( DiffComment diffComment : sortedDiffComments ) {
             
             int[] lineMappings = diffCommentLineMapper.getLineMappings(diffComment.getFirstLine());
             
@@ -82,32 +88,44 @@ public class ReviewModelFactory {
             
             ILineRange line = FACTORY.createLineRange();
             line.setStart(mappedLine);
-            line.setEnd(line.getStart() + ( diffComment.getNumLines() - 1) );
+            line.setEnd(line.getStart() + diffComment.getNumLines());
             
             ReviewboardCorePlugin.getDefault().trace(TraceLocation.MODEL, "Converted " +
             		"DiffComment [" + diffComment.getFirstLine()+ ", " + diffComment.getNumLines()+"] " +
     				"to ILineRange [" + line.getStart()+", " + line.getEnd()+"] on " + fileRevision.getPath() + " ( " + fileRevision.getRevision() + " )");
-            
+
             ILineLocation location = FACTORY.createLineLocation();
             location.getRanges().add(line);
             
+            ITopic topic = rangeToTopics.get(new Range(line));
+
             IUser author = createUser(diffComment.getUsername());
+            if ( topic == null ) {
+                topic = createTopic(diffComment, fileRevision, location, author);
+                rangeToTopics.put(new Range(line), topic);
+            }
             
             IComment topicComment = FACTORY.createComment();
             topicComment.setAuthor(author);
             topicComment.setCreationDate(diffComment.getTimestamp());
             topicComment.setDescription(diffComment.getText());
+
             
-            ITopic topic = FACTORY.createTopic();
-            topic.setId(String.valueOf(diffComment.getId()));
-            topic.setAuthor(author);
-            topic.setCreationDate(diffComment.getTimestamp());
-            topic.setLocation(location);
-            topic.setItem(fileRevision);
-            topic.setDraft(false);
-            topic.setDescription(diffComment.getText());
             topic.getComments().add(topicComment);
         }
+    }
+
+    private ITopic createTopic(DiffComment diffComment, IFileRevision fileRevision, ILineLocation location, IUser author) {
+        
+        ITopic topic = FACTORY.createTopic();
+        topic.setId(String.valueOf(diffComment.getId()));
+        topic.setAuthor(author);
+        topic.setCreationDate(diffComment.getTimestamp());
+        topic.setLocation(location);
+        topic.setItem(fileRevision);
+        topic.setDraft(false);
+        topic.setDescription(diffComment.getText());
+        return topic;
     }
 
     public IUser createUser(String username) {
@@ -120,5 +138,43 @@ public class ReviewModelFactory {
         else
             author.setDisplayName(username);
         return author;
+    }
+
+    private static class Range {
+        
+        private int _start;
+        private int _end;
+        
+        public Range(ILineRange range) {
+            _start = range.getStart();
+            _end = range.getEnd();
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + _end;
+            result = prime * result + _start;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Range other = (Range) obj;
+            if (_end != other._end)
+                return false;
+            if (_start != other._start)
+                return false;
+            return true;
+        }
+        
+        
     }
 }
