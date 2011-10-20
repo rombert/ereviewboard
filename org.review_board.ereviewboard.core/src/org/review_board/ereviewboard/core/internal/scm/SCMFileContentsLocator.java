191d132a5775883a64ea6e27d8a26c36f66a22fc
Robert Munteanu <robert.munteanu@gmail.com>
2011-10-20 15:32:17 +0300

    Improved documentation for extension points

--- org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/internal/scm/SCMFileContentsLocator.java
+++ org.review_board.ereviewboard.core/src/org/review_board/ereviewboard/core/internal/scm/SCMFileContentsLocator.java
@@ -12,25 +12,45 @@ package org.review_board.ereviewboard.core.internal.scm;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.review_board.ereviewboard.core.model.FileDiff;
 import org.review_board.ereviewboard.core.model.Repository;
 
 /**
  * The <tt>SCMFileContentsLocator</tt> is able to lookup the contents of a remote file at
  * a specified revision from a <tt>SCM</tt> system.
  * 
+ * <p>The methods are invoked as follows:</p>
+ * 
+ * <ol>
+ *   <li>the {@linkplain #init(Repository, String, String)}} method is invoked</li>
+ *   <li>the {@linkplain #isEnabled()} method is invoked</li>
+ *   <li>if the {@linkplain #isEnabled()} method returns true, the {@linkplain #getContents(IProgressMonitor)}
+ *   method is invoked</li>
+ * </ol>
+ * 
  * @author Robert Munteanu
- *
  */
 public interface SCMFileContentsLocator {
 
     /**
      * @param codeRepository the detected code repository, possibly <code>null</code>
-     * @param filePath the file path
-     * @param revision the file revision, <code>null</code> if the file does not exist in the <tt>SCM</tt> system
+     * @param filePath the file path in the SCM repository
+     * @param revision the file revision, possibly {@link FileDiff#PRE_CREATION}
      */
     void init(Repository codeRepository, String filePath, String revision);
     
+    /**
+     * @return true if this instance can retrieve the file contents from the parameters
+     * given to {@link #init(Repository, String, String)}
+     */
     boolean isEnabled();
     
+    /**
+     * Gets the file contents from SCM 
+     * 
+     * @param monitor
+     * @return the contents of the file retrieved from SCM, never <code>null</code>
+     * @throws CoreException if the contents can not be retrieved
+     */
     byte[] getContents(IProgressMonitor monitor) throws CoreException;
 }
--- org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ext/TaskDiffAction.java
+++ org.review_board.ereviewboard.ui/src/org/review_board/ereviewboard/ui/editor/ext/TaskDiffAction.java
@@ -17,23 +17,49 @@ import org.review_board.ereviewboard.core.ReviewboardDiffMapper;
 import org.review_board.ereviewboard.core.model.Repository;
 
 /**
+ * The <tt>TaskDiffAction</tt> is able to perform an action related to a submitted diff
+ * 
+ * <p>The methods are invoked as follows:</p>
+ * 
+ * <ol>
+ *   <li>the {@linkplain #init(TaskRepository, int, Repository, ReviewboardDiffMapper, Integer)}} method is invoked</li>
+ *   <li>the {@linkplain #isEnabled()} method is invoked</li>
+ *   <li>if the {@linkplain #isEnabled()} method returns true, the {@linkplain #execute(IProgressMonitor)}
+ *   method is invoked</li>
+ * </ol>
+ * 
  * @author Robert Munteanu
  * 
  */
+// TODO rename to ReviewRequestDiffAction
 public interface TaskDiffAction {
     
+    /**
+     * Signals that after the action is succesfully executed the locally cached review request data should be refreshed
+     */
     int STATUS_CODE_REFRESH_REVIEW_REQUEST = 1;
 
     /**
-     * @param repository
-     * @param reviewRequestId
-     * @param codeRepository
-     * @param diffMapper
+     * @param repository the task repository to which the review request is associated 
+     * @param reviewRequestId the id of the review request
+     * @param codeRepository the source code repository associated with the review request
+     * @param diffMapper the mapper which describes the diffs associated with the review request
      * @param diffRevisionId the diff revision id from this action, or null if applicable for all diff actions
      */
     void init(TaskRepository repository, int reviewRequestId, Repository codeRepository, ReviewboardDiffMapper diffMapper, Integer diffRevisionId);
 
+    /**
+     * @return true if this action can be executed for the properties given to {@link #init(TaskRepository, int, Repository, ReviewboardDiffMapper, Integer)}
+     */
     boolean isEnabled();
 
+    /**
+     * Executes this action
+     * 
+     * @param monitor
+     * @return the status corresponding to the result of the action
+     * 
+     * @see #STATUS_CODE_REFRESH_REVIEW_REQUEST
+     */
     IStatus execute(IProgressMonitor monitor);
 }
